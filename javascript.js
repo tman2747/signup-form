ebefc42c81bc5609cb474391ca4f944441aa6f49
Triston Land <tristonland@gmail.com>
2024-10-03 01:27:45 -0600

    border style changes on password text

--- javascript.js
+++ javascript.js
@@ -1,23 +1,22 @@
 let password1 = document.querySelector("#password");
 let password2 = document.querySelector("#passwordConfirm");
 let PasswordTEXT = document.querySelector("#PasswordTEXT")
-const createAccountButton = document.querySelector(".createAccountButton")
 
-console.log(createAccountButton.innerHTML)
 
 
 password2.addEventListener("input", (e)=>{
     if (password1.value !== password2.value)
     {
         PasswordTEXT.style.opacity = 1;
-        createAccountButton.style.opacity = 0;
-        password2.style.border = 'rgb(255, 0, 0) 2px solid';
+        password2.style.border = 'rgb(255, 0, 0) 1px solid';
+        password1.style.border = 'rgb(255, 0, 0) 1px solid';
 
     }
     else
     {
         PasswordTEXT.style.opacity = 0;
-        createAccountButton.style.opacity = 1;
+        password2.style.border = 'rgb(0, 0, 0) 1px solid';
+        password1.style.border = 'rgb(0, 0, 0) 1px solid';
 
     }
 })
